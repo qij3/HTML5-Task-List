87ac424517c94c5da1af4941c8f56cb9a1fc1c7c
arun197 <tarunshrivatsav@gmail.com>
2012-09-26 03:42:39 +0530

    Added buttons and event listeners

--- DOMNode.js
+++ DOMNode.js
@@ -4,35 +4,52 @@ begin=true;
 /* Global array to retrieve and store the 'localstorage' values locally*/
 var value= new array();
 
-/** Retrieves the localStorage Values **/
+/**To Create date for each task**/
+function adddate()
+{
+
+	var mydate= new Date()
+	var time = mydate.toLocaleString();	
+	return time;
+}
 
 
+/**Retrieves the stored Tasks from 'localstorage' values **/
 function retrieve()
 {
-var key=localStorage.key(0);
-value=localStorage[key];
+var key1=localStorage.key(0);
+value=localStorage[key1];
 value = value.split(",");		
+
 }
 
-function createtask(nodevalue)
+/**Adds the button 'Clear' and 'Completed'
+**/
+function addbutton(list)
 {
-	var show=document.getElementById("show-items");
-	var list=document.createElement('li');
-	var evt = document.createEvent("MouseEvent");
-	evt.initMouseEvent("onclick",true,true);
-	list.addEventListener("click",clicks,false);	
-	var textnode=document.createTextNode(nodevalue);
-	list.appendChild(textnode);
-	show.appendChild(list); 
-
-}
+ var button1 = document.createElement("input");
+ var button2 = document.createElement("input");
  
-function clicks()
+    //Assign different attributes to the element.
+    button1.setAttribute("type","button");
+    button2.setAttribute("type","button");
+    button1.setAttribute("value","Completed");
+    button2.setAttribute("value","Clear");
+ 
+    button1.addEventListener('click',completed,true);
+    button2.addEventListener('click',cleartask,true);
+    list.appendChild(button1);
+    list.appendChild(button2);
+}
+
+/**An event listener for clear button. Clears the task and removes the data from local storage**/
+function cleartask()
 
 {		
-	var textnode= this.firstChild;
+	var node=this.parentNode;
+	var textnode= node.firstChild;
 	var show=document.getElementById("show-items");
-	show.removeChild(this);
+	show.removeChild(node);
 	
 	/* retrieve() --> commented since value array is global **/
 	
@@ -50,6 +67,53 @@ function clicks()
 	
 }
 
+/**Removes the data from localstorage and adds the data in 'Completed' Column**/
+
+function completed()
+{			
+		
+	var time= adddate();	
+	var node=this.parentNode;
+	var text = node.firstChild;		
+	var app=text.data+"("+time+")";		
+	var complete=document.getElementById("completed");
+	var list=document.createElement('li');		
+	var textnode=document.createTextNode(app);
+	list.appendChild(textnode);	
+	complete.appendChild(list); 
+	
+	var node=this.parentNode;
+	var textnode= node.firstChild;
+	var show=document.getElementById("show-items");
+	show.removeChild(node);
+	
+	/* retrieve() --> commented since value array is global **/
+	
+	for(var i=0; i<value.length; i++)
+	{
+		if(value[i]==textnode.data)
+		{
+			value.splice(i,1);					
+			localStorage.setItem("itemId", value);								
+			break;		
+		}	
+			
+	}	
+}
+
+/**Creates the element 'li' and adds the task data to it**/
+function createtask(nodevalue)
+{
+	var show=document.getElementById("show-items");
+	var list=document.createElement('li');		
+	var textnode=document.createTextNode(nodevalue);
+	list.appendChild(textnode);
+	addbutton(list);	
+	show.appendChild(list); 
+
+}
+ 
+/**Retrieves the contents from localStorage and creates the corresponding html elements**/
 function start()
 {
 							
@@ -67,20 +131,14 @@ function start()
 	
 }
 
-
-
+/**Pushes the task values to localStorage**/
 function Addtask()
 {
 	
-	var mydate= new Date()
-	var time = mydate.toLocaleString();	
-	var theyear=mydate.getFullYear()
-	var themonth=mydate.getMonth()+1
-	var thetoday=mydate.getDate()
-	var today=time+"/"+themonth+"/"+thetoday+"/"+theyear;
-	
+	var time= adddate();	
 	var task=document.getElementById("task");	
 	var app=task.value+"("+time+")";	
+	task.value="";	
 	value.push(app);			
 	
 	try{
@@ -88,7 +146,7 @@ function Addtask()
 	}
 	catch(e)
 	{}
-
+	
 	createtask(app);
 		
 }
